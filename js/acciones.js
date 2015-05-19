afd143e0bb0f9cad9dd7045f365b7eaf4aac9aed
AL31F13 <laura.Ocelotl.ponce@gmail.com>
2015-05-19 14:02:57 -0500

    uno

---
+++ js/acciones.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+	{
+		$('#uno').on('click', function(){
+			$('#total').append('1')
+		});
+		$('#dos').on('click', function(){
+			$('#total').append('2')
+		});
+		$('#tres').on('click', function(){
+			$('#total').append('3')
+		});
+		$('#cuatro').on('click', function(){
+			$('#total').append('4')
+		});
+	}
+	
+//}); 
+});
+ 
